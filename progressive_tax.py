b046cfa9301a4fdf2c7ac0bc8dc271748daee468
John Lloyd <johnl5124@gmail.com>
2021-12-14 16:34:45 +0000

    added link to origin reddit post and a class for more structure

--- progressive_tax.py
+++ progressive_tax.py
@@ -1,29 +1,31 @@
 #reddit [easy] challenge - progressive taxation
+#link https://www.reddit.com/r/dailyprogrammer/comments/cdieag/20190715_challenge_379_easy_progressive_taxation/
 
-#below are the tax rate brackets
-income_tax_0 = 0
-income_tax_1 = 0.10
-income_tax_2 = 0.25
-income_tax_3 = 0.40
+class progressive_tax():
+    #below are the tax rate brackets
+    income_tax_0 = 0
+    income_tax_1 = 0.10
+    income_tax_2 = 0.25
+    income_tax_3 = 0.40
 
-income = 56789
-tax = 0
+    income = 56789
+    tax = 0
 
-#0% tax
-if income < 10000:
-    print(income * income_tax_0)
-#10% tax
-if income >= 10000 and income < 30000:
-    print((income - 10000) * income_tax_1)
-#25% tax
-if income >= 30000 and income < 100000:
-    income_10_30 = 30000 * income_tax_1
-    print(income_10_30 + (income - 30000) * income_tax_2)
-#40% tax
-#if income > 
+    #0% tax
+    if income < 10000:
+        print(income * income_tax_0)
+    #10% tax
+    if income >= 10000 and income < 30000:
+        print((income - 10000) * income_tax_1)
+    #25% tax
+    if income >= 30000 and income < 100000:
+        income_10_30 = 30000 * income_tax_1
+        print(income_10_30 + (income - 30000) * income_tax_2)
+    #40% tax
+    #if income > 
 
-#income
-jim_income = 7500
-doris_income = 25000
-gordon_income = 75000
-michael_income = 650000
+    #income
+    jim_income = 7500
+    doris_income = 25000
+    gordon_income = 75000
+    michael_income = 650000
