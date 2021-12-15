15856b1dea04d3b3445cfd43c885102a457eecf2
John Lloyd <johnl5124@gmail.com>
2021-12-15 12:40:26 +0000

    completed EASY reddit challenge of progressive taxation

--- progressive_tax.py
+++ progressive_tax.py
@@ -2,30 +2,20 @@
 #link https://www.reddit.com/r/dailyprogrammer/comments/cdieag/20190715_challenge_379_easy_progressive_taxation/
 
 class progressive_tax():
-    #below are the tax rate brackets
-    income_tax_0 = 0
-    income_tax_1 = 0.10
-    income_tax_2 = 0.25
-    income_tax_3 = 0.40
 
-    income = 56789
-    tax = 0
+    income = 1234567
 
     #0% tax
     if income < 10000:
-        print(income * income_tax_0)
+        print("Your tax on: £", income, "is £", income * 0)
     #10% tax
     if income >= 10000 and income < 30000:
-        print((income - 10000) * income_tax_1)
+        print("Your tax on: £", income, "is £", (income-10000) * 0.1)
     #25% tax
-    if income >= 30000 and income < 100000:
-        income_10_30 = 30000 * income_tax_1
-        print(income_10_30 + (income - 30000) * income_tax_2)
+    if income >= 30000 and income <= 100000:
+        print("Your tax on: £", income, "is £", ((income - 10000) * 0.1) + ((income - 30000) * 0.15)) 
     #40% tax
-    #if income > 
-
-    #income
-    jim_income = 7500
-    doris_income = 25000
-    gordon_income = 75000
-    michael_income = 650000
+    if income > 100000:
+        print("Your tax on: £", income, "is £", ((income-10000) * 0.10) + ((income - 30000) * 0.15) + ((income - 100000) * 0.15))
+        
+ 
